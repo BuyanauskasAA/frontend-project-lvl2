f4e9da33aed8511773b093f73ba50a9bc1d459e9
BuyanauskasAA <Buyanauskas@gmail.com>
2022-04-28 19:47:35 +0300

    step #3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import findDiff from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
@@ -8,4 +9,8 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const diff = findDiff(filepath1, filepath2);
+    console.log(diff);
+  })
   .parse();
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { cwd } from 'process';
+import _ from 'lodash';
+import path from 'path';
+import fs from 'fs';
+
+export default (filepath1, filepath2) => {
+  const file1 = JSON.parse(fs.readFileSync(path.resolve(cwd(), filepath1), 'utf8'));
+  const file2 = JSON.parse(fs.readFileSync(path.resolve(cwd(), filepath2), 'utf8'));
+
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
+  const keys = _.uniq([...keys1, ...keys2]).sort();
+
+  const result = keys.reduce((acc, key) => {
+    if (!keys2.includes(key)) {
+      acc.push(`  - ${key}: ${file1[key]}`);
+    } else if (!keys1.includes(key)) {
+      acc.push(`  + ${key}: ${file2[key]}`);
+    } else {
+      if (file1[key] === file2[key]) {
+        acc.push(`    ${key}: ${file1[key]}`)
+      } else {
+        acc.push(`  - ${key}: ${file1[key]}`);
+        acc.push(`  + ${key}: ${file2[key]}`);
+      } 
+    }
+
+    return acc;
+  }, []);
+
+  return `{\n${result.join('\n')}\n}`;
+};
