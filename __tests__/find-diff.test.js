bdba3646f88e865d658a17586b81e129f8ec13ba
BuyanauskasAA <Buyanauskas@gmail.com>
2022-04-30 23:03:55 +0300

    test yaml

--- __tests__/find-diff.test.js
+++ __tests__/find-diff.test.js
@@ -10,10 +10,17 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const filepath1 = getFixturePath('file1.json');
-const filepath2 = getFixturePath('file2.json');
+const jsonFilepath1 = getFixturePath('file1.json');
+const jsonFilepath2 = getFixturePath('file2.json');
+
+const yamlFilepath1 = getFixturePath('file1.yml');
+const yamlFilepath2 = getFixturePath('file2.yml');
 const expectedFile = readFile('expected_file');
 
-test('find difference', () => {
-  expect(findDiff(filepath1, filepath2)).toEqual(expectedFile);
+test('generate difference .json', () => {
+  expect(findDiff(jsonFilepath1, jsonFilepath2)).toEqual(expectedFile);
 });
+
+test('generate difference .yaml', () => {
+  expect(findDiff(yamlFilepath1, yamlFilepath2)).toEqual(expectedFile);
+})
