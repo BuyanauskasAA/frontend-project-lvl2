2ed8a5215bfada12a7481a143e8c23c58734c8eb
BuyanauskasAA <Buyanauskas@gmail.com>
2022-05-26 17:36:53 +0300

    refactoring stylish.js

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,54 +1,52 @@
 import _ from 'lodash';
 
+const replacer = {
+  oldValueBar: '  - ',
+  newValueBar: '  + ',
+  notEditedBar: '    ',
+  space: '    ',
+};
+
+const formatedValue = (value, depth) => {
+  const { space } = replacer;
+  const indent = space.repeat(depth);
+  if (!_.isObject(value)) {
+    return `${value}`;
+  }
+  const lines = Object
+    .entries(value)
+    .map(([key, val]) => `${indent}${space}${key}: ${formatedValue(val, depth + 1)}`);
+  return [
+    '{',
+    ...lines,
+    `${indent}}`,
+  ].join('\n');
+};
+
 const stylish = (diff) => {
-  const replacer = {
-    oldValueBar: '  - ',
-    newValueBar: '  + ',
-    notEditedBar: '    ',
-  };
-
   const iter = (elements, depth) => {
-    const space = '    ';
+    const {
+      oldValueBar, newValueBar, notEditedBar, space,
+    } = replacer;
     const indent = space.repeat(depth);
-    const { oldValueBar, newValueBar, notEditedBar } = replacer;
-
-    const formatedValue = (currentValue, currentDepth = depth + 1) => {
-      const currentIndent = space.repeat(currentDepth);
-      if (!_.isObject(currentValue)) {
-        return `${currentValue}`;
-      }
-      const result = Object
-        .entries(currentValue)
-        .map(([key, val]) => `${currentIndent}${notEditedBar}${key}: ${formatedValue(val, currentDepth + 1)}`);
-      return [
-        '{',
-        ...result,
-        `${currentIndent}}`,
-      ].join('\n');
-    };
-
     const lines = _.sortBy(elements, ((element) => element.name))
       .flatMap((element) => {
         const {
-          name,
-          status,
-          value,
-          oldValue,
-          newValue,
-          children,
+          name, status, value, oldValue, newValue, children,
         } = element;
+        const currentValue = formatedValue(value, depth + 1);
 
         switch (status) {
           case 'deleted':
-            return `${indent}${oldValueBar}${name}: ${formatedValue(value)}`;
+            return `${indent}${oldValueBar}${name}: ${currentValue}`;
           case 'added':
-            return `${indent}${newValueBar}${name}: ${formatedValue(value)}`;
+            return `${indent}${newValueBar}${name}: ${currentValue}`;
           case 'not-edited':
-            return `${indent}${notEditedBar}${name}: ${formatedValue(value)}`;
+            return `${indent}${notEditedBar}${name}: ${currentValue}`;
           case 'edited':
             return [
-              `${indent}${oldValueBar}${name}: ${formatedValue(oldValue)}`,
-              `${indent}${newValueBar}${name}: ${formatedValue(newValue)}`,
+              `${indent}${oldValueBar}${name}: ${formatedValue(oldValue, depth + 1)}`,
+              `${indent}${newValueBar}${name}: ${formatedValue(newValue, depth + 1)}`,
             ];
           case 'internal-values':
             return `${indent}${notEditedBar}${name}: ${iter(children, depth + 1)}`;
